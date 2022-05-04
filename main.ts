2c84a02302eae491f99629b4c9d4a09869741404
Captain Nick Lucifer* <naryal2580@gmail.com>
2022-05-04 19:58:49 +0545

    misc changes

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 import puppeteer from "https://deno.land/x/puppeteer/mod.ts"; // 9.0.2
-// import { sleep } from "https://deno.land/x/sleep/mod.ts";
+import { sleep } from "https://deno.land/x/sleep/mod.ts";
 import {readLines} from "https://deno.land/std/io/bufio.ts";
 
 const token = config()['TOKEN'];
 const browser = await puppeteer.launch({ 'headless': false });
 const page = await browser.newPage();
 const app_url = 'https://discord.com/app';
+const invite_urls_f = await Deno.open('./inviteURLS.txt');
 
 page.on('console', msg => { console.log(msg.text()) });
 
@@ -19,15 +20,19 @@ await page.evaluate((token) => {
     let elem = document.createElement("iframe");
     document.body.appendChild(elem).contentWindow.localStorage.token = '"${token}"';
   }, 50); setTimeout(() => { location.reload() }, 2500);`);
-}, token)
+}, token);
 
 
-page.waitForTimeout(2e3);
-const invite_urls_f=await Deno.open('./inviteURLS.txt');
+console.log('\n\nHERE\n\n')
+await sleep(2e3);
+
+var new_tab = null;
+console.log('\n\nHERE\n\n')
+
 for await( const url of readLines(invite_urls_f) ) {
-  const new_tab = await browser.newPage();
-  await new_tab.goto(url);
   console.log('\n\n'+ url +'\n\n')
+  new_tab = await browser.newPage();
+  await new_tab.goto(url);
   await new_tab.evaluate( () => {
     eval(`setTimeout(() => {
           document.querySelectorAll('input')[0].click();
@@ -36,6 +41,6 @@ for await( const url of readLines(invite_urls_f) ) {
           document.querySelectorAll('button')[1].click();
         }, 4444);`);
   });
-}
+};
 
 // Deno.exit()
